0d1458e4bf75aa730a693118cb6568059fe78cb6
Anna Kuliapina <Lina_89_05@mail.ru>
2022-11-15 16:39:14 +0500

    Добавляет сообщения при отклике сервера

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 import browserHistory from './browser-history';
 import App from './components/app/app';
 import HistoryRouter from './components/history-route/history-route';
 import { store } from './store';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -14,6 +16,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <HistoryRouter history={browserHistory} >
+        <ToastContainer />
         <App />
       </HistoryRouter>
     </Provider>
--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -1,5 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { AxiosInstance } from 'axios';
+import { toast } from 'react-toastify';
 import { APIRoute, MAX_CAMERAS_OF_PAGE, OrderType, QueryParams, SortType } from '../const';
 import { Camera, Cameras, FetchCamerasPayloadType } from '../types/cameras-type';
 import { Promo } from '../types/promo-type';
@@ -13,23 +14,27 @@ export const fetchCamerasAction = createAsyncThunk<{data: Cameras; camerasCount:
 }>(
   'data/fetchCameras',
   async ({pageId, paramsSort}, {extra: api}) => {
-    const {data, headers} = await api.get<Cameras>(APIRoute.Cameras, {
-      params: {
-        [QueryParams.Limit]: MAX_CAMERAS_OF_PAGE,
-        [QueryParams.Page]: pageId,
-        [QueryParams.Order]: paramsSort._order,
-        [QueryParams.Sort]: paramsSort._sort,
-        [QueryParams.Category]: paramsSort.category,
-        [QueryParams.Type]: paramsSort.type,
-        [QueryParams.Level]: paramsSort.level,
-        [QueryParams.MinPrice]: paramsSort.minPrice,
-        [QueryParams.MaxPrice]: paramsSort.maxPrice,
-      }
-    });
-    return {
-      data,
-      camerasCount: headers['x-total-count']
-    };
+    try {
+      const {data, headers} = await api.get<Cameras>(APIRoute.Cameras, {
+        params: {
+          [QueryParams.Limit]: MAX_CAMERAS_OF_PAGE,
+          [QueryParams.Page]: pageId,
+          [QueryParams.Order]: paramsSort._order,
+          [QueryParams.Sort]: paramsSort._sort,
+          [QueryParams.Category]: paramsSort.category,
+          [QueryParams.Type]: paramsSort.type,
+          [QueryParams.Level]: paramsSort.level,
+          [QueryParams.MinPrice]: paramsSort.minPrice,
+          [QueryParams.MaxPrice]: paramsSort.maxPrice,
+        }
+      });
+      return {
+        data,
+        camerasCount: headers['x-total-count']};
+    } catch (error) {
+      toast.error('Unable to load cameras. Try again later');
+      throw (error);
+    }
   }
 );
 
@@ -46,31 +51,37 @@ export const fetchCamerasOfMinMaxPrice = createAsyncThunk<{ minPriceOfCameras: n
 }>(
   'data/fetchCamerasOgMinMaxPrice',
   async ({params}, {extra: api}) => {
-    const responseCameraMinPrice = await api.get<Cameras>(APIRoute.Cameras, {
-      params: {
-        [QueryParams.Order]: OrderType.Asc,
-        [QueryParams.Sort]: SortType.Price,
-        [QueryParams.Category]: params.category,
-        [QueryParams.Type]: params.type,
-        [QueryParams.Level]: params.level,
-        [QueryParams.Limit]: 1
-      }
-    });
-    const responseCameraMaxPrice = await api.get<Cameras>(APIRoute.Cameras, {
-      params: {
-        [QueryParams.Order]: OrderType.Desc,
-        [QueryParams.Sort]: SortType.Price,
-        [QueryParams.Category]: params.category,
-        [QueryParams.Type]: params.type,
-        [QueryParams.Level]: params.level,
-        [QueryParams.Limit]: 1
-      }
-    });
+    try {
+      const responseCameraMinPrice = await api.get<Cameras>(APIRoute.Cameras, {
+        params: {
+          [QueryParams.Order]: OrderType.Asc,
+          [QueryParams.Sort]: SortType.Price,
+          [QueryParams.Category]: params.category,
+          [QueryParams.Type]: params.type,
+          [QueryParams.Level]: params.level,
+          [QueryParams.Limit]: 1
+        }
+      });
+      const responseCameraMaxPrice = await api.get<Cameras>(APIRoute.Cameras, {
+        params: {
+          [QueryParams.Order]: OrderType.Desc,
+          [QueryParams.Sort]: SortType.Price,
+          [QueryParams.Category]: params.category,
+          [QueryParams.Type]: params.type,
+          [QueryParams.Level]: params.level,
+          [QueryParams.Limit]: 1
+        }
+      });
 
-    return {
-      minPriceOfCameras: responseCameraMinPrice.data[0].price,
-      maxPriceOfCameras: responseCameraMaxPrice.data[0].price,
-    };
+      return {
+        minPriceOfCameras: responseCameraMinPrice.data[0].price,
+        maxPriceOfCameras: responseCameraMaxPrice.data[0].price,
+      };
+    } catch (error) {
+
+      toast.error('Failed to load prices. Try again later');
+      throw (error);
+    }
   }
 );
 
@@ -81,8 +92,13 @@ export const fetchPromoCameraAction = createAsyncThunk<Promo, undefined, {
 }>(
   'data/fetchPromoCamera',
   async (_arg, {extra: api}) => {
-    const {data} = await api.get<Promo>(APIRoute.Promo);
-    return data;
+    try {
+      const {data} = await api.get<Promo>(APIRoute.Promo);
+      return data;
+    } catch (error) {
+      toast.error('Unable to load promo cameras. Try again later');
+      throw (error);
+    }
   }
 );
 
@@ -93,8 +109,13 @@ export const fetchCameraByIdAction = createAsyncThunk<Camera, number, {
 }>(
   'data/fetchCameraById',
   async (id, {extra: api}) => {
-    const {data} = await api.get<Camera>(`${APIRoute.Cameras}/${id}`);
-    return data;
+    try {
+      const {data} = await api.get<Camera>(`${APIRoute.Cameras}/${id}`);
+      return data;
+    } catch (error) {
+      toast.error('Unable to load camera. Try again later');
+      throw(error);
+    }
   }
 );
 
@@ -105,8 +126,13 @@ export const fetchSimilarCamerasAction = createAsyncThunk<Cameras, number, {
 }>(
   'data/fetchSimilarCameras',
   async (id, {extra: api}) => {
-    const {data} = await api.get<Cameras>(`${APIRoute.Cameras}/${id}/similar`);
-    return data;
+    try {
+      const {data} = await api.get<Cameras>(`${APIRoute.Cameras}/${id}/similar`);
+      return data;
+    } catch (error) {
+      toast.error('Failed to load similar cameras. Try again later');
+      throw(error);
+    }
   },
 );
 
@@ -117,8 +143,13 @@ export const fetchReviewsAction = createAsyncThunk<Reviews, number, {
 }>(
   'data/fetchReviews',
   async (id, {extra: api}) => {
-    const {data} = await api.get<Reviews>(`${APIRoute.Cameras}/${id}/reviews`);
-    return data;
+    try {
+      const {data} = await api.get<Reviews>(`${APIRoute.Cameras}/${id}/reviews`);
+      return data;
+    } catch (error) {
+      toast.error('Failed to load reviews. Try again later');
+      throw(error);
+    }
   },
 );
 
@@ -129,8 +160,13 @@ export const addReviewAction = createAsyncThunk<void, ReviewPost, {
 }>(
   'data/addReview',
   async(review, {dispatch, extra: api}) => {
-    await api.post<Review>(APIRoute.Review, review);
-    dispatch(fetchReviewsAction(review.cameraId));
+    try {
+      await api.post<Review>(APIRoute.Review, review);
+      dispatch(fetchReviewsAction(review.cameraId));
+    } catch (error) {
+      toast.error('Failed to add review. Try again later');
+      throw(error);
+    }
   },
 );
 
@@ -141,7 +177,12 @@ export const fetchCamerasByNameAction = createAsyncThunk<Cameras, string, {
 }>(
   'data/fetchCamerasByName',
   async (name, {extra: api}) => {
-    const {data} = await api.get<Cameras>(APIRoute.Cameras, {params: {[QueryParams.NameLike]: name}});
-    return data;
+    try {
+      const {data} = await api.get<Cameras>(APIRoute.Cameras, {params: {[QueryParams.NameLike]: name}});
+      return data;
+    } catch (error) {
+      toast.error('Failed to find cameras. Try again later');
+      throw(error);
+    }
   }
 );
