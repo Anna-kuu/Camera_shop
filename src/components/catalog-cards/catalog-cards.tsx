e131ce4955981230eafd760e14719458ed5647bb
Anna Kuliapina <Lina_89_05@mail.ru>
2022-12-06 17:05:40 +0500

    Кнопка В корзине

--- src/components/catalog-cards/catalog-cards.tsx
+++ src/components/catalog-cards/catalog-cards.tsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom';
-import { APIRoute, MAX_RATING} from '../../const';
+import { APIRoute, AppRoute, MAX_RATING} from '../../const';
 import { resetCounter } from '../../store/reviews-data/reviews-data';
 import { useAppDispatch } from '../../hooks/use-app-dispatch';
 import { Camera, Cameras } from '../../types/cameras-type';
+import { getCamerasInBascket } from '../../store/basket-data/selectors';
+import { useAppSelector } from '../../hooks/use-app-selector';
 
 type CatalogCardsPropsType = {
   cameras: Cameras;
@@ -12,42 +14,53 @@ type CatalogCardsPropsType = {
 
 export default function CatalogCards({cameras, setSelectedCamera, setIsModalAddItemActiv}: CatalogCardsPropsType):JSX.Element {
   const dispatch = useAppDispatch();
+  const camerasInBasket = useAppSelector(getCamerasInBascket);
   const handleButtonAddItemClick = (camera: Camera) => {
     setSelectedCamera(camera);
     setIsModalAddItemActiv(true);
     document.body.style.overflow = 'hidden';
   };
 
-  const camerasCatalog = cameras.map((camera) => (
-    <div className="product-card" key={`product-card-${camera.id}`}>
-      <div className="product-card__img">
-        <picture>
-          <source type="image/webp" srcSet={`/${camera.previewImgWebp}, /${camera.previewImgWebp2x} 2x`} />
-          <img src={camera.previewImg} srcSet={`${camera.previewImg2x} 2x`} width="280" height="240" alt={camera.name} />
-        </picture>
-      </div>
-      <div className="product-card__info">
-        <div className="rate product-card__rate">
-          {Array.from({length: MAX_RATING}, (it, index) => (
-            <svg width="17" height="16" aria-hidden="true" key={`star-${index}`}>
-              <use xlinkHref={`#icon-${index < camera.rating ? 'full-' : ''}star`}></use>
-            </svg>
-          ))}
-          <p className="visually-hidden">{`Рейтинг: ${camera.rating}`}</p>
-          <p className="rate__count"><span className="visually-hidden">Всего оценок:</span>{camera.reviewCount}</p>
+  const camerasCatalog = cameras.map((camera) => {
+    const isCameraInBasket = camerasInBasket.find((cameraInBasket) => camera.id === cameraInBasket.camera.id);
+    return (
+      <div className="product-card" key={`product-card-${camera.id}`}>
+        <div className="product-card__img">
+          <picture>
+            <source type="image/webp" srcSet={`/${camera.previewImgWebp}, /${camera.previewImgWebp2x} 2x`} />
+            <img src={camera.previewImg} srcSet={`${camera.previewImg2x} 2x`} width="280" height="240" alt={camera.name} />
+          </picture>
         </div>
-        <p className="product-card__title">{camera.name}</p>
-        <p className="product-card__price"><span className="visually-hidden">Цена:</span>{`${camera.price.toLocaleString('ru')} ₽`}
-        </p>
+        <div className="product-card__info">
+          <div className="rate product-card__rate">
+            {Array.from({length: MAX_RATING}, (it, index) => (
+              <svg width="17" height="16" aria-hidden="true" key={`star-${index}`}>
+                <use xlinkHref={`#icon-${index < camera.rating ? 'full-' : ''}star`}></use>
+              </svg>
+            ))}
+            <p className="visually-hidden">{`Рейтинг: ${camera.rating}`}</p>
+            <p className="rate__count"><span className="visually-hidden">Всего оценок:</span>{camera.reviewCount}</p>
+          </div>
+          <p className="product-card__title">{camera.name}</p>
+          <p className="product-card__price"><span className="visually-hidden">Цена:</span>{`${camera.price.toLocaleString('ru')} ₽`}
+          </p>
+        </div>
+        <div className="product-card__buttons">
+          {isCameraInBasket ?
+            <Link className="btn btn--purple-border product-card__btn product-card__btn--in-cart" to={AppRoute.Basket}>
+              <svg width="16" height="16" aria-hidden="true">
+                <use xlinkHref="#icon-basket"></use>
+              </svg>В корзине
+            </Link>
+            :
+            <button onClick={() => handleButtonAddItemClick(camera)} className="btn btn--purple product-card__btn" type="button">Купить
+            </button>}
+          <Link onClick={() => dispatch(resetCounter())} className="btn btn--transparent" to={`${APIRoute.Cameras}/${camera.id}?tab=specification`}>Подробнее
+          </Link>
+        </div>
       </div>
-      <div className="product-card__buttons">
-        <button onClick={() => handleButtonAddItemClick(camera)} className="btn btn--purple product-card__btn" type="button">Купить
-        </button>
-        <Link onClick={() => dispatch(resetCounter())} className="btn btn--transparent" to={`${APIRoute.Cameras}/${camera.id}?tab=specification`}>Подробнее
-        </Link>
-      </div>
-    </div>
-  ));
+    );}
+  );
 
   return (
     <div className="cards catalog__cards">
--- src/components/similar-cameras/similar-cameras.tsx
+++ src/components/similar-cameras/similar-cameras.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { APIRoute, MAX_RATING, SLIDER_DEFAULT, SLIDER_STEP } from '../../const';
+import { APIRoute, AppRoute, MAX_RATING, SLIDER_DEFAULT, SLIDER_STEP } from '../../const';
 import { useAppDispatch } from '../../hooks/use-app-dispatch';
+import { useAppSelector } from '../../hooks/use-app-selector';
+import { getCamerasInBascket } from '../../store/basket-data/selectors';
 import { resetCounter } from '../../store/reviews-data/reviews-data';
 import { Camera, Cameras } from '../../types/cameras-type';
 
@@ -13,6 +15,7 @@ type SimilarCamerasProps = {
 
 export default function SimilarCameras({similarCameras, setSelectedCamera, setIsModalAddItemActiv}: SimilarCamerasProps):JSX.Element {
   const dispatch = useAppDispatch();
+  const camerasInBasket = useAppSelector(getCamerasInBascket);
   const handleButtonAddItemClick = (camera: Camera) => {
     setSelectedCamera(camera);
     setIsModalAddItemActiv(true);
@@ -20,36 +23,46 @@ export default function SimilarCameras({similarCameras, setSelectedCamera, setIs
   };
   const [sliderStart, setSliderStart] = useState(SLIDER_DEFAULT);
   const shownSimilarCameras = similarCameras.slice(sliderStart, (sliderStart + SLIDER_STEP));
-  const similarCamerasSlider = shownSimilarCameras.map((camera) => (
-    <div className="product-card is-active" key={`slider-${camera.id}`}>
-      <div className="product-card__img">
-        <picture>
-          <source type="image/webp" srcSet={`/${camera.previewImgWebp}, /${camera.previewImgWebp2x} 2x`}/>
-          <img src={camera.previewImg} srcSet={`${camera.previewImg2x} 2x`} width="280" height="240" alt={camera.name}/>
-        </picture>
-      </div>
-      <div className="product-card__info">
-        <div className="rate product-card__rate">
-          {Array.from({length: MAX_RATING}, (it, index) => (
-            <svg width="17" height="16" aria-hidden="true" key={`star-${index}`}>
-              <use xlinkHref={`#icon-${index < camera.rating ? 'full-' : ''}star`}></use>
-            </svg>
-          ))}
-          <p className="visually-hidden">Рейтинг: {camera.rating}</p>
-          <p className="rate__count"><span className="visually-hidden">Всего оценок:</span>{camera.reviewCount}</p>
+  const similarCamerasSlider = shownSimilarCameras.map((camera) => {
+    const isCameraInBasket = camerasInBasket.find((cameraInBasket) => camera.id === cameraInBasket.camera.id);
+    return (
+      <div className="product-card is-active" key={`slider-${camera.id}`}>
+        <div className="product-card__img">
+          <picture>
+            <source type="image/webp" srcSet={`/${camera.previewImgWebp}, /${camera.previewImgWebp2x} 2x`}/>
+            <img src={camera.previewImg} srcSet={`${camera.previewImg2x} 2x`} width="280" height="240" alt={camera.name}/>
+          </picture>
         </div>
-        <p className="product-card__title">{camera.name}</p>
-        <p className="product-card__price"><span className="visually-hidden">Цена:</span>{camera.price.toLocaleString('ru')} ₽
-        </p>
+        <div className="product-card__info">
+          <div className="rate product-card__rate">
+            {Array.from({length: MAX_RATING}, (it, index) => (
+              <svg width="17" height="16" aria-hidden="true" key={`star-${index}`}>
+                <use xlinkHref={`#icon-${index < camera.rating ? 'full-' : ''}star`}></use>
+              </svg>
+            ))}
+            <p className="visually-hidden">Рейтинг: {camera.rating}</p>
+            <p className="rate__count"><span className="visually-hidden">Всего оценок:</span>{camera.reviewCount}</p>
+          </div>
+          <p className="product-card__title">{camera.name}</p>
+          <p className="product-card__price"><span className="visually-hidden">Цена:</span>{camera.price.toLocaleString('ru')} ₽
+          </p>
+        </div>
+        <div className="product-card__buttons">
+          {isCameraInBasket ?
+            <Link className="btn btn--purple-border product-card__btn product-card__btn--in-cart" to={AppRoute.Basket}>
+              <svg width="16" height="16" aria-hidden="true">
+                <use xlinkHref="#icon-basket"></use>
+              </svg>В корзине
+            </Link>
+            :
+            <button onClick={() => handleButtonAddItemClick(camera)} className="btn btn--purple product-card__btn" type="button">Купить
+            </button>}
+          <Link onClick={() => dispatch(resetCounter())} className="btn btn--transparent" to={`${APIRoute.Cameras}/${camera.id}`}>Подробнее
+          </Link>
+        </div>
       </div>
-      <div className="product-card__buttons">
-        <button onClick={() => handleButtonAddItemClick(camera)} className="btn btn--purple product-card__btn" type="button">Купить
-        </button>
-        <Link onClick={() => dispatch(resetCounter())} className="btn btn--transparent" to={`${APIRoute.Cameras}/${camera.id}`}>Подробнее
-        </Link>
-      </div>
-    </div>
-  ));
+    );}
+  );
   return (
     <section className="product-similar">
       <div className="container">
